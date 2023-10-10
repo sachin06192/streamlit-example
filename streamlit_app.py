e0ed44f3fc4e84fb090acf5d3f64d5bc70bfb34b
sachin06192 <33274650+sachin06192@users.noreply.github.com>
2023-10-11 00:03:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,13 @@ import pymongo
 import streamlit as st
 
 # Connect to MongoDB Atlas
-client = pymongo.MongoClient("mongodb+srv://sachintest:JZHyLgBIIFngFHcP@cluster0.wmki6co.mongodb.net/")
+#client = pymongo.MongoClient("mongodb+srv://sachintest:JZHyLgBIIFngFHcP@cluster0.wmki6co.mongodb.net/")
 
-if client.server_info():
-    print("MongoDB connection established successfully.")
-
-# Access a specific database
-db = client["Cluster0"]
-
-# Access a specific collection
-collection = db["streamlit.sample-data"]
+try:
+    # Attempt to establish a connection
+    client = pymongo.MongoClient("mongodb+srv://sachintest:JZHyLgBIIFngFHcP@cluster0.wmki6co.mongodb.net/")
+    st.write("MongoDB connection established successfully.")
+except pymongo.errors.ConnectionFailure as e:
+    # Handle connection failure
+    st.error(f"MongoDB connection error: {e}")
 
