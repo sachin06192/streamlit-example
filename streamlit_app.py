c03adbc4f78724fe737f6c3c997a20e1a5e16f5c
sachin06192 <33274650+sachin06192@users.noreply.github.com>
2023-10-11 00:00:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,12 @@ import streamlit as st
 # Connect to MongoDB Atlas
 client = pymongo.MongoClient("mongodb+srv://sachintest:JZHyLgBIIFngFHcP@cluster0.wmki6co.mongodb.net/")
 
+if client.server_info():
+    print("MongoDB connection established successfully.")
+
 # Access a specific database
 db = client["Cluster0"]
 
 # Access a specific collection
 collection = db["streamlit.sample-data"]
-
-# Example: Query data from MongoDB and display it in your Streamlit app
-cursor = collection.find({"name":"Jane Doe"})
-if cursor.count_documents({"name":"Jane Doe"}) == 0:
-    st.write("No documents found.")
-else:
-    data = list(cursor)
-    for document in data:
-        st.write(document)
 
