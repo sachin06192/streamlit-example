50ff01376d3988672a2ad81cf43762593e6e6c77
sachin06192 <33274650+sachin06192@users.noreply.github.com>
2023-10-11 00:11:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,15 @@ try:
     # Attempt to establish a connection
     client = pymongo.MongoClient("mongodb+srv://sachintest:JZHyLgBIIFngFHcP@cluster0.wmki6co.mongodb.net/")
     st.write("MongoDB connection established successfully.")
+    # Access a specific database
+    db = client["testdb"]
+    # Access a specific collection
+    collection = db["testcollection"]
+
+    cursor = collection.find({"name":"sachin"})
+    data = list(cursor)
+    for document in data:
+        st.write(document)
 except pymongo.errors.ConnectionFailure as e:
     # Handle connection failure
     st.error(f"MongoDB connection error: {e}")
